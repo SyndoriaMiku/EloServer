abf6218487482eaf24e61a21710a01b6620a04ec
Syndoria <61247021+SyndoriaMiku@users.noreply.github.com>
2024-02-07 18:04:39 +0700

    Update urls.py

--- Elo/urls.py
+++ Elo/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('players/<int:pk>/', elo.views.PlayerDetail.as_view(), name='player-detail'),
     path('result/', elo.views.ResultView.as_view(), name='result'),
     path('draw/', elo.views.DrawView.as_view(), name='draw'),
+    path('login/', elo.views.LoginView.as_view(), name='login'),
     path('new/', elo.views.GetNewIDView.as_view(), name='new'),
     path('filterPlayers/', elo.views.PlayerFilterView.as_view(), name='filter-players'), 
-    path('login/', elo.views.LoginView.as_view(), name='login'),
 ]
 
